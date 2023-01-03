8bf58f7796e7c9a0b660b9ac324c6a1e8f5b86f8
Aditya Ramesh Mysore <adityamysore002@yahoo.com>
2023-01-02 23:34:58 -0500

    First commit

---
+++ src/test/java/com/springfirst/first/FirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springfirst.first;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
